9be01e905496f7da6bcb8fe39ba8fef1fc9bc421
Berner Setterwall <berner.setterwall@campanja.com>
2015-04-22 16:19:24 +0200

    Init commit.

---
+++ pd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+THE_RAND=$(echo -n $RANDOM)
+
+mv $(TARGET) $(TARGET)_$THE_RAND
+git remote add origin-deploy $GITREMOTE
+git fetch origin-deploy
+git checkout deploy
+git config --global user.email $EMAIL
+git config --global user.name "Build service"
+date > generated
+rm -r $(TARGET)
+mv $(TARGET)_$THE_RAND $(TARGET)
+git add generated $(TARGET)
+git commit -m 'new release'
+git push -u origin-deploy deploy
