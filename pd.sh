5826c12175e399b029839f10e9eeac70e1fc9af6
Berner Setterwall <berner.setterwall@campanja.com>
2015-05-11 11:34:04 +0200

    Use {} not ().

--- pd.sh
+++ pd.sh
@@ -2,15 +2,15 @@
 
 THE_RAND=$(echo -n $RANDOM)
 
-mv $(TARGET) $(TARGET)_$THE_RAND
+mv ${TARGET} ${TARGET}_$THE_RAND
 git remote add origin-deploy $GITREMOTE
 git fetch origin-deploy
 git checkout deploy
 git config --global user.email $EMAIL
 git config --global user.name "Build service"
 date > generated
-rm -r $(TARGET)
-mv $(TARGET)_$THE_RAND $(TARGET)
-git add generated $(TARGET)
+rm -r $TARGET
+mv ${TARGET}_$THE_RAND $TARGET
+git add generated $TARGET
 git commit -m 'new release'
 git push -u origin-deploy deploy
