6f78ea41f3ffda168cbcea1351315e17fc8fb6bb
Berner Setterwall <berner.setterwall@campanja.com>
2015-05-13 13:27:48 +0200

    Clean .gitignores ..

--- pd.sh
+++ pd.sh
@@ -11,6 +11,8 @@ git config --global user.name "Build service"
 date > generated
 rm -fr $TARGET
 mv ${TARGET}_$THE_RAND $TARGET
+# get rid of any .gitignore shadowing usefull files.
+find $TARGET -name '.gitignore' -exec rm {} \;
 git add generated $TARGET
 git commit -m 'new release'
 git push -u origin-deploy deploy
